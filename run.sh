756935a53fc14f6aa3ccf6d08988e681a992d210
TaocsZhang <89051869+TaocsZhang@users.noreply.github.com>
2021-10-15 15:27:43 +1100

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,102 @@
+
+
+'''
+python train.py --alpha 0.1
+
+python train.py --alpha 0.2
+
+python train.py --alpha 0.3
+
+python train.py --alpha 0.4
+
+python train.py --alpha 0.5
+
+python train.py --alpha 0.6
+
+python train.py --alpha 0.7
+
+python train.py --alpha 1
+
+python train.py --alpha 0.9
+
+
+python train.py --alpha 1.1
+python train.py --alpha 1.2
+
+python train.py --alpha 1.3
+python train.py --alpha 1.4
+python train.py --alpha 1.5
+python train.py --alpha 1.6
+python train.py --alpha 1.7
+
+
+python train.py --alpha 2
+python train.py --alpha 3
+
+python train.py --alpha 5
+python train.py --alpha 6
+python train.py --alpha 7
+
+python train.py --alpha 1
+
+
+
+python train.py --alpha 2
+
+python train.py --alpha 2.2
+python train.py --alpha 2.4
+python train.py --alpha 2.6
+python train.py --alpha 2.8
+
+python train.py --alpha 3
+
+python train.py --alpha 5
+python train.py --alpha 6
+python train.py --alpha 7
+python train.py --alpha 8
+python train.py --alpha 9
+python train.py --alpha 10
+python train.py --alpha 11
+
+
+python train.py --alpha 20
+'''
+
+python train.py --num_unlabel 100
+
+python train.py --num_unlabel 200
+
+python train.py --num_unlabel 300
+
+python train.py --num_unlabel 400
+
+#python train.py --num_unlabel 4000
+
+#python train.py --num_unlabel 6000
+
+#python train.py --num_unlabel 8000
+
+'''
+1. experiment 1: varying alpha
+
+bank dataset: 2000 unlabeled data
+health dataset: 5000 unlabeled data
+titanic dataset: 388 unlabeled data
+
+Weight decay = 0.01 for three datasets,
+
+
+alpha in the titanic dataset
+
+dp: 0.1-1;2-7
+fpr: around 1
+
+
+
+2. varying the size of unlabeled data
+ alpha = 0.1 in bank dataset, alpha = 0.5 in titanic dataset, alpha = 0.8 in health dataset
+
+
+
+
+'''
